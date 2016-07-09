9f831658c6af06101a722cec604e366fc29c3bc2
Juruken <donte.wyrmfoe@gmail.com>
2016-07-09 21:35:03 +1000

    Fix permutations calculator.

--- Kiwiland/Calculators/TripPermutationsCalculator.cs
+++ Kiwiland/Calculators/TripPermutationsCalculator.cs
@@ -28,7 +28,7 @@ namespace Kiwiland.Calculators
             return CalculatePermutations(sourceStation, destinationStation, maximumDistance);
         }
 
-        internal List<Trip> CalculatePermutations(string sourceStation, string destinationStation, int maximumDistance)
+        private List<Trip> CalculatePermutations(string sourceStation, string destinationStation, int maximumDistance)
         {
             ValidateStationsExist(sourceStation, destinationStation);
 
@@ -37,7 +37,6 @@ namespace Kiwiland.Calculators
 
             var tripsByTripName = new Dictionary<string, Trip>();
 
-            // Loop of each route to see if they can get to our required destination.
             foreach (var route in source.Routes.Values)
             {
                 var currentTrip = new Trip
@@ -46,14 +45,7 @@ namespace Kiwiland.Calculators
                     TripName = sourceStation + route.DestinationStation
                 };
 
-                // TODO: DRY... fix this.
-                var trips = GeneratorTrip(currentTrip, maximumDistance, route.DestinationStation, destinationStation);
-
-                foreach (var trip in trips.Values)
-                {
-                    if (!tripsByTripName.ContainsKey(trip.TripName))
-                        tripsByTripName.Add(trip.TripName, trip);
-                }
+                GeneratorTrip(tripsByTripName, currentTrip, maximumDistance, route.DestinationStation, destinationStation);
             }
 
             var result = tripsByTripName.Values.ToList();
@@ -63,35 +55,27 @@ namespace Kiwiland.Calculators
             return result;
         }
 
-        internal Dictionary<string, Trip> GeneratorTrip(Trip currentTrip, int maximumDistance, string currentStation, string destinationStation)
+        private void GeneratorTrip(Dictionary<string, Trip> tripsByTripName, Trip currentTrip, int maximumDistance, string currentStation, string destinationStation)
         {
             if (currentTrip.TotalDistance > maximumDistance)
-                return null;
+                return;
 
-            var tripsByTripName = new Dictionary<string, Trip>();
             var current = m_StationProvider.GetStation(currentStation);
             
             foreach (var route in current.Routes.Values)
             {
-                currentTrip.TotalDistance += route.Distance;
-                currentTrip.TripName += route.DestinationStation;
+                var newTrip = currentTrip.Clone();
+                newTrip.TotalDistance += route.Distance;
+                newTrip.TripName += route.DestinationStation;
                 
-                if (currentTrip.TotalDistance >= maximumDistance)
+                if (newTrip.TotalDistance >= maximumDistance)
                     continue;
                 
-                if (currentTrip.TripName.EndsWith(destinationStation) && !tripsByTripName.ContainsKey(currentTrip.TripName))
-                    tripsByTripName.Add(currentTrip.TripName, currentTrip);
+                if (newTrip.TripName.EndsWith(destinationStation) && !tripsByTripName.ContainsKey(newTrip.TripName))
+                    tripsByTripName.Add(newTrip.TripName, newTrip.Clone());
 
-                var trips = GeneratorTrip(currentTrip, maximumDistance, route.DestinationStation, destinationStation);
-
-                foreach (var trip in trips.Values)
-                {
-                    if (!tripsByTripName.ContainsKey(trip.TripName))
-                        tripsByTripName.Add(trip.TripName, trip);
-                }
+                GeneratorTrip(tripsByTripName, newTrip.Clone(), maximumDistance, route.DestinationStation, destinationStation);
             }
-
-            return tripsByTripName;
         }
 
         internal void ValidateStationsExist(string sourceStation, string destinationStation)
--- Kiwiland/Data/Trip.cs
+++ Kiwiland/Data/Trip.cs
@@ -4,5 +4,14 @@
     {
         public string TripName { get; set; } 
         public int TotalDistance { get; set; }
+
+        public Trip Clone()
+        {
+            return new Trip()
+            {
+                TotalDistance = TotalDistance,
+                TripName = TripName
+            };
+        }
     }
 }
